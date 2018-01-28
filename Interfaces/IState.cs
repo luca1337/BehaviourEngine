f61ffbcc02f73a00803e4af85e2b32aacd800b10
SamueleAlpino <samuelealpino@gmail.com>
2018-01-28 18:02:22 +0100

    ++

--- Interfaces/IState.cs
+++ Interfaces/IState.cs
@@ -8,6 +8,7 @@ namespace BehaviourEngine.Interfaces
 {
     public interface IState
     {
+        GameObject Owner {get;set;}
         void OnStateEnter();
         void OnStateExit();
         IState OnStateUpdate();
